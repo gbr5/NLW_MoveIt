9a2a82aa8037343e7c0a955dc1852e1051203fcd
GuyBR <gbrobbe@gmail.com>
2021-02-27 05:01:37 -0300

    Fix: level variable bug

--- src/contexts/ChallengeContext.tsx
+++ src/contexts/ChallengeContext.tsx
@@ -18,11 +18,9 @@ export const ChallengesProvider: React.FC<ChallengesProviderProps> = (
     children,
     ...rest
   }: ChallengesProviderProps) => {
-  const [level, setLevel] = useState(rest.level ?? 1);
-  const [experienceToNextLevel, setExperienceToNextLevel] = useState(Math.pow((level + 1) * 4, 2));
-  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
-  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
-
+  const [level, setLevel] = useState((Number(Cookies.get('level')) || rest.level) ?? 1);
+  const [currentExperience, setCurrentExperience] = useState((Number(Cookies.get('currentExperience')) || rest.currentExperience) ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState((Number(Cookies.get('challengesCompleted')) || rest.challengesCompleted) ?? 0);
   const [activeChallenge, setActiveChallenge] = useState(null);
   const [isLevelUpModelOpen, setIsLevelUpModelOpen] = useState(false);
 
@@ -30,18 +28,18 @@ export const ChallengesProvider: React.FC<ChallengesProviderProps> = (
     Notification.requestPermission();
   }, []);
 
-  useEffect(() => {
-    Cookies.set('level', String(level));
-    Cookies.set('currentExperience', String(currentExperience));
-    Cookies.set('challengesCompleted', String(challengesCompleted));
-    setExperienceToNextLevel(Math.pow((level + 1) * 4, 2));
-  }, [level, currentExperience, challengesCompleted]);
-
   const levelUp = useCallback(() => {
-    Cookies.set('level', String(level + 1));
-    setLevel(level + 1);
+    const thisLevel = Number(Cookies.get('level'));
+    Cookies.set('level', String(thisLevel + 1));
+    setLevel(thisLevel + 1);
     setIsLevelUpModelOpen(true);
-  }, [level])
+  }, [])
+
+  const updateChallengesCompleted = useCallback(() => {
+    const thisChallengesCompleted = Number(Cookies.get('challengesCompleted'));
+    Cookies.set('challengesCompleted', String(thisChallengesCompleted + 1));
+    setChallengesCompleted(thisChallengesCompleted + 1);
+  }, []);
 
   const closeLevelUpModal = useCallback(() => {
     setIsLevelUpModelOpen(false);
@@ -70,28 +68,26 @@ export const ChallengesProvider: React.FC<ChallengesProviderProps> = (
   }, []);
 
   const completeChallenge = useCallback(() => {
-    const completedChallenge = Cookies.get('challenge');
-    const completedChallenges = Cookies.get('challengesCompleted');
-    if (completedChallenge === null || completedChallenge === undefined) {
+    const challenge = Cookies.get('challenge');
+    if (challenge === null || challenge === undefined) {
       return;
     }
-    const thisChallenge: IChallengeDTO = JSON.parse(completedChallenge);
+    const thisChallenge: IChallengeDTO = JSON.parse(challenge);
     const { amount } = thisChallenge;
-    const experience = Cookies.get('currentExperience');
-    const finalExperience = Number(experience) + amount;
+    const thisLevel = Number(Cookies.get('level'));
+    const experienceToNextLevel = Math.pow((thisLevel + 1) * 4, 2);
+    const currentExperienceUpdated = Number(Cookies.get('currentExperience'));
+    const finalExperience = currentExperienceUpdated + amount;
 
     if (finalExperience >= experienceToNextLevel) {
-      Cookies.set('challengesCompleted', String(Number(completedChallenges) + 1));
-      setChallengesCompleted(Number(completedChallenges) + 1);
       Cookies.set('currentExperience', String(finalExperience - experienceToNextLevel));
       setCurrentExperience(finalExperience - experienceToNextLevel);
       levelUp();
     } else {
-      Cookies.set('challengesCompleted', String(Number(completedChallenges) + 1));
-      setChallengesCompleted(Number(completedChallenges) + 1);
       Cookies.set('currentExperience', String(finalExperience));
       setCurrentExperience(finalExperience);
     }
+    updateChallengesCompleted();
     resetChallenge();
   }, []);
 
@@ -114,7 +110,7 @@ export const ChallengesProvider: React.FC<ChallengesProviderProps> = (
       value={{
         closeLevelUpModal,
         resetMemory,
-        experienceToNextLevel,
+        experienceToNextLevel: Math.pow((level + 1) * 4, 2),
         level,
         activeChallenge,
         currentExperience,
